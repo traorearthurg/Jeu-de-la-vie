f25e6dc834b8c4818aedb0418ac4c4c70e87711e
traorearthurg <traore.arthurg@gmail.com>
2026-04-11 02:33:24 +0200

    feat: ajout du comptage des voisins

--- main.py
+++ main.py
@@ -26,6 +26,22 @@ class JeuDeLaVie:
             grille_principale.append(nouvelle_ligne)
         return grille_principale
 
+    def compter_voisins(self, ligne, colonne):
+        compteur = 0
+        for i in [-1, 0, 1]:
+            for j in [-1, 0, 1]:
+                if i == 0 and j == 0:
+                    continue
+                if ligne+i <0 or ligne+i == self.lignes:
+                    continue
+                if colonne+j <0 or colonne+j ==self.colonnes:
+                    continue
+                if self.grille[ligne+i][colonne+j] == 1:
+                    compteur+=1
+        return compteur
+
+
+
 
     def afficher_console(self):
         """Affiche la grille dans le terminal pour tester."""
@@ -39,6 +55,7 @@ class JeuDeLaVie:
 if __name__ == "__main__":
     # On crée une "instance" (un objet physique) à partir de notre plan (la classe)
     mon_jeu = JeuDeLaVie(10, 10)
-
     # On demande à notre objet de s'afficher
     mon_jeu.afficher_console()
+
+print("Voisins de la case (2,2) :", mon_jeu.compter_voisins(2, 2))
